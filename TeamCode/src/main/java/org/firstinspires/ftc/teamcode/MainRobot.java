f7a8de4deb9b562cf0bb7b0e93abd23b29578eea
Maxim <maximvanier@gmail.com>
2024-09-19 20:41:18 -0400

    Make drive work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
@@ -2,9 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.acmerobotics.dashboard.FtcDashboard;
 
 @TeleOp(name = "Main Robot")
 public class MainRobot extends LinearOpMode {
@@ -13,6 +12,17 @@ public class MainRobot extends LinearOpMode {
     private double rot;
     private double vx;
     private double vy;
+    int FAST_FORWARD_FACTOR = 1400;
+    int SLOW_FORWARD_FACTOR = 1000;
+    int FAST_SIDEWAYS_FACTOR = 1200;
+    int SLOW_SIDEWAYS_FACTOR = 720;
+    int FAST_ROTATION_FACTOR = 1300;
+    int SLOW_ROTATION_FACTOR = 900;
+
+    // Gamepads
+    public Gamepad g1;
+    public Gamepad g2;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -30,23 +40,32 @@ public class MainRobot extends LinearOpMode {
     }
     private void initialize() {
         common = new RobotCommon(hardwareMap);
-//        common.initialize();
-//        dashboard = FtcDashboard.getInstance();
-//        telemetry = dashboard.getTelemetry();
-//
-//        // Gamepad
-//        g1 = new Gamepad();
-//        g2 = new Gamepad();
+        common.initialize();
+
+        telemetry = FtcDashboard.getInstance().getTelemetry();
+
+        // Gamepad
+        g1 = new Gamepad();
+        g2 = new Gamepad();
     }
 
 
     private void controls() {
+        g1.copy(gamepad1);
+        g2.copy(gamepad2);
 
+        boolean movingFastSideways = g1.a && Math.abs(g1.left_stick_x) > Math.abs(g1.left_stick_y);
+        boolean movingFastForward = g1.a && Math.abs(g1.left_stick_y) > Math.abs(g1.left_stick_x);
+        vx = movingFastSideways ? 0 : -square(g1.left_stick_y) * (g1.a ?  FAST_FORWARD_FACTOR : SLOW_FORWARD_FACTOR);
+        vy = movingFastForward ? 0 : square(g1.left_stick_x) * (g1.a ? FAST_SIDEWAYS_FACTOR : SLOW_SIDEWAYS_FACTOR);
+        rot = square(g1.right_trigger - g1.left_trigger) * (g1.a ? FAST_ROTATION_FACTOR : SLOW_ROTATION_FACTOR);
+        common.setRobotSpeed(vx, vy, rot);
     }
     private void sendTelemetry() {
     }
-
-
+    public static double square(double amount) {
+        return amount * Math.abs(amount);
+    }
 }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCommon.java
@@ -1,30 +1,79 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.hardware.lynx.LynxModule;
+
+import java.util.List;
 
 public class RobotCommon {
     private final HardwareMap hardwareMap;
+
+    private DcMotorEx frontLeft;
+    private DcMotorEx frontRight;
+    private DcMotorEx backLeft;
+    private DcMotorEx backRight;
+    private double vx;
+    private double vy;
+    private double rot;
 
     public RobotCommon(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
     }
 
-    public void runDrive() {
-//        double wheel1;
-//        double wheel2;
-//        double wheel3;
-//        double wheel4;
-//
-//        wheel1 = vx + vy + rot;
-//        wheel2 = (vx - vy) - rot;
-//        wheel3 = (vx - vy) + rot;
-//        wheel4 = (vx + vy) - rot;
-//        frontLeft.setVelocity(wheel1);
-//        frontRight.setVelocity(wheel2);
-//        backLeft.setVelocity(wheel3);
-//        backRight.setVelocity(wheel4);
+    public void initialize() {
+        // Bulk Read
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        // Init hardware
+        frontLeft = hardwareMap.get(DcMotorEx.class, "Front Left");
+        frontRight = hardwareMap.get(DcMotorEx.class, "Front Right");
+        backLeft = hardwareMap.get(DcMotorEx.class, "Back Left");
+        backRight = hardwareMap.get(DcMotorEx.class, "Back Right");
+
+        // Config Motors
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        // Enable Encoders
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void run() {
+        runDrive();
+    }
+
+    public void setRobotSpeed(double vx, double vy, double rot) {
+    this.vx = vx;
+    this.vy = vy;
+    this.rot = rot;
+    }
+
+    private void runDrive() {
+        double wheel1;
+        double wheel2;
+        double wheel3;
+        double wheel4;
+
+        wheel1 = vx + vy + rot;
+        wheel2 = (vx - vy) - rot;
+        wheel3 = (vx - vy) + rot;
+        wheel4 = (vx + vy) - rot;
+        frontLeft.setVelocity(wheel1);
+        backLeft.setVelocity(wheel2);
+        frontRight.setVelocity(wheel3);
+        backRight.setVelocity(wheel4);
     }
 }
